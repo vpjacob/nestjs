cb70fb14b162e71bd2c3174f2678b55ab3d8d6fc
董建新 <824629419@qq.com>
2019-09-05 14:14:06 +0800

    swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,14 +1,34 @@
 import { Controller ,Post,Get} from '@nestjs/common';
+import { ApiUseTags, ApiModelProperty, ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 
+export class Cat {
+    @ApiModelProperty()
+    name: string;
+  
+    @ApiModelProperty()
+    age: number;
+  
+    @ApiModelProperty()
+    breed: string;
+  }
+  
 @Controller('post')
+@ApiUseTags('document')
 export class PostController {
 
     @Get()
-    post(){
-        return 'get'
+    @ApiOperation({title:'title',description:'desc'})
+    index(){
+        return [
+            {index:1,title:2},
+            {index:1,title:2},
+            {index:1,title:2},
+            {index:1,title:2},
+        ]
     }
 
     @Post()
+    @ApiCreatedResponse({ description: 'The record has been successfully created.', type: Cat })
     create(){
         return 'post';
     }
